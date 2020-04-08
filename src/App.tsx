3d8f81de235c4cb5f2cc114a04362afc7b3709f8
Martin Davis <martin_davis3@yahoo.ca>
2020-04-08 12:19:45 -0600

    add some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
-import Something from './Something';
-import LikeButton from './LikeButton';
-
+import {Button, Icon, Card, Image} from 'semantic-ui-react';  //NB you can put multiple objects for import!
+// import {Icon} from 'semantic-ui-react';
 function App() {
 
   return (
     <React.Fragment>
       <Fragment>
-        <Fragment>
-          <h1>This is a header!</h1>
-          <p>Paragraph!<em>Emphasized text!</em></p>
-          <button className="SpecialButton">I know what a button is!</button>
-          <button>Another button!</button>
-          <p>Proof!</p>
-        </Fragment>
-        <Fragment>
-          <h1>This is a header!</h1>
-          <p>Paragraph!<em>Emphasized text!</em></p>
-          <button className="SpecialButton">I know what a button is!</button>
-          <button>Another button!</button>
-          <p>Proof!</p>
-        </Fragment>
+        <h1>This is a header!</h1>
+        <p>Paragraph!<em>Emphasized text!</em></p>
+        <button className="SpecialButton">I know what a button is!</button>
+        <button>Another button!</button>
+        <p>Proof!</p>
+        <Button color='red'>
+        <Icon name='heart' />
+        Like
+        </Button>
+        <Card>
+          <Card.Content>
+            <Image
+              floated='right'
+              size='mini'
+              src='../public/img/favicon.png'
+            />
+            <Card.Header>Steve Sanders</Card.Header>
+            <Card.Meta>Friends of Elliot</Card.Meta>
+            <Card.Description>
+              Steve wants to add you to the group <strong>best friends</strong>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+              <Button basic color='green'>
+                Approve
+              </Button>
+              <Button basic color='red'>
+                Decline
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
       </Fragment>
     </React.Fragment>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Something from './Something';
-import { Button, Icon, Label } from 'semantic-ui-react';
 
 ReactDOM.render(
   <React.StrictMode>
